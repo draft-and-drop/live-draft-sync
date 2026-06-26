b8c9815d8a87032f4e10858d2c0eba115b17f065
dreaesposito <dreaesposito@hotmail.com>
2026-06-25 22:14:23 -0500

    add script to map FantasyPros IDs to Sleeper IDs

---
+++ scripts/create-fp-sleeper-map.ts
@@ -0,0 +1,38 @@
+import { readFile, mkdir, writeFile } from "node:fs/promises"
+import Papa from "papaparse"
+
+interface PlayerIdRow {
+  fantasypros_id?: string
+  sleeper_id?: string
+}
+
+const csv = await readFile("player_ids.csv", "utf8")
+
+const { data, errors } = Papa.parse<PlayerIdRow>(csv, {
+  header: true,
+  skipEmptyLines: true
+})
+
+if (errors.length) {
+  console.warn("CSV parsing errors:", errors)
+}
+
+const playerIdMap = Object.fromEntries(
+  data
+    .filter(row => row.fantasypros_id && row.sleeper_id)
+    .map(row => [
+      row.fantasypros_id!,
+      row.sleeper_id!
+    ])
+)
+
+await mkdir("server/data", { recursive: true })
+
+await writeFile(
+  "server/data/fp-sleeper-id-map.json",
+  JSON.stringify(playerIdMap, null, 2)
+)
+
+console.log(
+  `Created ${Object.keys(playerIdMap).length} mappings`
+)
