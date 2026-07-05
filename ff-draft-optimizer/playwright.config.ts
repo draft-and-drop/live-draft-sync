cb6e8b01e3b2c42d3778e83972a4455ae17886d5
dreaesposito <dreaesposito@hotmail.com>
2026-07-05 15:44:21 -0500

    update playwright/ts config and clean up

--- ff-draft-optimizer/playwright.config.ts
+++ ff-draft-optimizer/playwright.config.ts
@@ -13,6 +13,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './test',
+  timeout: 30000,
+  expect: {
+    timeout: 30000
+  },
+  use: {
+    actionTimeout: 5000,
+    trace: 'on-first-retry'
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -39,15 +47,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
