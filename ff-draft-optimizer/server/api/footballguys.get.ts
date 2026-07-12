a61e76d4e9ab570f3d7db4a722010db7472ecbb5
dreaesposito <dreaesposito@hotmail.com>
2026-07-12 12:49:39 -0500

    update football guys rank source

--- ff-draft-optimizer/server/api/footballguys.get.ts
+++ ff-draft-optimizer/server/api/footballguys.get.ts
@@ -3,33 +3,33 @@ import playerIdMapJson from "#server/data/fg-sleeper-id-map.json";
 
 type FootballGuysPlayerDTO = {
   player_name: string;
+  position: string;
   team: string;
   football_guys_id: string;
   sleeper_id: string | null;
 };
 
 const playerIdMap = playerIdMapJson as Record<string, string>;
+const MAX_ENTRIES = 300;
 
 export default defineEventHandler(async () => {
   const htmlString = await $fetch<string>(
-    "https://www.footballguys.com/adp?season=2026&pos=all",
+    "https://www.footballguys.com/rankings#more",
   );
 
   const root = parse(htmlString);
 
   const players: FootballGuysPlayerDTO[] = root
-    .querySelectorAll("tr")
-    .slice(1)
+    .querySelectorAll("tr[data-playerid][data-rank][data-playername]")
+    .slice(0, MAX_ENTRIES)
     .map((tr) => {
-      const tds = tr.querySelectorAll("td");
-      const a = tds[1]?.querySelector("a");
-      const href = a?.getAttribute("href") ?? "";
-
-      const footballGuysId = href.split("/").at(-1)?.replace("+", ".") ?? "";
+      const spans = tr.querySelectorAll('[class^="pos-"], [class^="team-abbr"]');
+      const footballGuysId = tr?.getAttribute("data-playerid") ?? "";
 
       return {
-        player_name: a?.text.trim() ?? "",
-        team: tds[3]?.text.trim().split("/")[0]?.trim() ?? "",
+        player_name: tr.getAttribute("data-playername") ?? "",
+        team: spans[0]?.innerText ?? "",
+        position: spans[1]?.innerText.replace(/[0-9]/g, "") ?? "",
         football_guys_id: footballGuysId,
         sleeper_id: playerIdMap[footballGuysId] ?? null,
       };
